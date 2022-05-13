22321bc6ee26fc002430f5c8de9a2684c653769a
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2022-05-13 19:34:03 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import "./Banner.css"
+import React from "react";
+import "./Banner.css";
 
 const Banner = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200" style="background-image: url(https://api.lorem.space/image/fashion?w=1000&h=800);">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src="https://api.lorem.space/image/movie?w=260&h=400"
+            className="max-w-sm rounded-lg shadow-2xl" alt=""
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
